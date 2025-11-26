47f8a622964ebd92f1a51e1c31af737c467308e8
julijanlaznik <98269924+julijanlaznik@users.noreply.github.com>
2025-11-26 21:04:51 +0100

    Change video source to placeholder in Hero component
    Replaced YouTube video source with a placeholder.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -411,7 +411,7 @@ const Hero: React.FC = () => {
                     <iframe 
                         width="100%" 
                         height="100%" 
-                        src="https://www.youtube.com/embed/LXb3EKWsInQ?autoplay=1&rel=0&modestbranding=1&controls=0" 
+                        src="#" 
                         title="Smart Digital Showreel" 
                         frameBorder="0" 
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
